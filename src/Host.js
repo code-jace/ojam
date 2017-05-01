bf714f5144986b95a9e272d46f50cc0780b30f8d
Oisín Hennessy <crowregent@gmail.com>
2017-05-02 00:15:37 +0100

    Routing

---
+++ src/Host.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Header from './components/Header'
+//import GenericComponent from './components/GenericComponent'
+import Chat from './components/Chat'
+import InfoTitle from './components/InfoTitle'
+import InfoProgressBar from './components/InfoProgressBar'
+import YoutubeVideo from './components/YoutubeVideo'
+import Controls from './components/Controls'
+import Search from './components/Search'
+import Veto from './components/Veto'
+import List from './components/List'
+import InfoUsers from './components/InfoUsers'
+
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'
+import 'bootstrap-sass/assets/javascripts/bootstrap.min.js'
+
+
+
+
+
+export default class Host extends React.Component {
+  render () {
+    return <div className=''>
+      <h1>Ojam! Democratic Social Music System</h1>
+      <h2>Host Application</h2>
+      <InfoTitle />
+      <InfoProgressBar />
+      <YoutubeVideo />
+      <Controls />
+      <Veto />
+      <Search />
+      <List />
+      <InfoUsers />
+      <p>
+        
+      </p>
+
+      
+    </div>
+  }
+}
